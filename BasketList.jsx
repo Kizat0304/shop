56ea906e89129c0e1b66f0feed471c5e9d5d0a55
Kizat0304 <104340827+Kizat0304@users.noreply.github.com>
2022-04-25 11:11:57 +0600

    Add files via upload

---
+++ BasketList.jsx
@@ -0,0 +1,51 @@
+import { BasketItem } from './BasketItem';
+
+function BasketList(props) {
+    const {
+        order = [],
+        handleBasketShow = Function.prototype,
+        removeFromBasket = Function.prototype,
+        incQuantity,
+        decQuantity,
+    } = props;
+
+    const totalPrice = order.reduce((sum, el) => {
+        return sum + el.price * el.quantity;
+    }, 0);
+
+    return (
+        <ul className='collection basket-list'>
+            <li className='collection-item active'>Корзина</li>
+            {order.length ? (
+                order.map((item) => (
+                    <BasketItem
+                        key={item.id}
+                        removeFromBasket={removeFromBasket}
+                        incQuantity={incQuantity}
+                        decQuantity={decQuantity}
+                        {...item}
+                    />
+                ))
+            ) : (
+                <li className='collection-item'>Корзина пуста</li>
+            )}
+            <li className='collection-item active'>
+                1 token = 1 Тенге.
+            </li>
+            <li className='collection-item active'>
+                Общая стоимость: {totalPrice} Тенге.
+            </li>
+            <li className='collection-item'>
+                <button className='btn btn-small'>Оформить</button>
+            </li>
+            <i
+                className='material-icons basket-close'
+                onClick={handleBasketShow}
+            >
+                close
+            </i>
+        </ul>
+    );
+}
+
+export { BasketList };
---
+++ config.js
@@ -0,0 +1,5 @@
+const API_KEY = 'fb0ae017-2938fe5f-ac30681d-6c83cc3f';
+
+const API_URL = 'https://fortniteapi.io/v1/shop?lang=ru';
+
+export { API_KEY, API_URL };
